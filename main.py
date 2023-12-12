90e53cf5ae20639bfe11224644813a2796f9ffcd
Iris <f14106155@gs.ncku.edu.tw>
2023-12-12 17:16:30 +0800

    偵測coins畫出邊緣

--- main.py
+++ main.py
@@ -4,31 +4,20 @@ import numpy as np
 
 
 def main(argv):
-    ## [load]
     default_file = "coins.jpg"
     filename = argv[0] if len(argv) > 0 else default_file
-
     # Loads an image
     src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
-
     # Check if image is loaded fine
     if src is None:
         print("Error opening image!")
         print("Usage: hough_circle.py [image_name -- default " + default_file + "] \n")
         return -1
-    ## [load]
 
-    ## [convert_to_gray]
-    # Convert it to gray
     gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-    ## [convert_to_gray]
 
-    ## [reduce_noise]
-    # Reduce the noise to avoid false circle detection
     gray = cv.medianBlur(gray, 5)
-    ## [reduce_noise]
 
-    ## [houghcircles]
     rows = gray.shape[0]
     circles = cv.HoughCircles(
         gray,
@@ -40,9 +29,7 @@ def main(argv):
         minRadius=1,
         maxRadius=30,
     )
-    ## [houghcircles]
 
-    ## [draw]
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
@@ -51,13 +38,10 @@ def main(argv):
             cv.circle(src, center, 1, (0, 100, 100), 3)
             # circle outline
             radius = i[2]
-            cv.circle(src, center, radius, (255, 0, 255), 3)
-    ## [draw]
+            cv.circle(src, center, radius, (0, 255, 0), 3)
 
-    ## [display]
     cv.imshow("detected circles", src)
     cv.waitKey(0)
-    ## [display]
 
     return 0
 
