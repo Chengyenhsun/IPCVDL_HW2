fb334ffab225d33b4f0096f81e5c910a0d976c24
Iris <f14106155@gs.ncku.edu.tw>
2023-12-12 17:48:47 +0800

    改成GaussianBlur

--- 1.1.py
+++ 1.1.py
@@ -1,5 +1,5 @@
 import sys
-import cv2 as cv
+import cv2
 import numpy as np
 
 
@@ -7,27 +7,28 @@ def main(argv):
     default_file = "coins.jpg"
     filename = argv[0] if len(argv) > 0 else default_file
     # Loads an image
-    src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+    src = cv2.imread(cv2.samples.findFile(filename), cv2.IMREAD_COLOR)
     # Check if image is loaded fine
     if src is None:
         print("Error opening image!")
         print("Usage: hough_circle.py [image_name -- default " + default_file + "] \n")
         return -1
 
-    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-    gray = cv.medianBlur(gray, 5)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    cv2.imshow("gray", gray)
 
     rows = gray.shape[0]
-    circles = cv.HoughCircles(
+    circles = cv2.HoughCircles(
         gray,
-        cv.HOUGH_GRADIENT,
+        cv2.HOUGH_GRADIENT,
         1,
-        rows / 8,
+        rows / 16,
         param1=100,
         param2=30,
-        minRadius=1,
-        maxRadius=30,
+        minRadius=10,
+        maxRadius=40,
     )
 
     if circles is not None:
@@ -35,13 +36,13 @@ def main(argv):
         for i in circles[0, :]:
             center = (i[0], i[1])
             # circle center
-            cv.circle(src, center, 1, (0, 100, 100), 3)
+            cv2.circle(src, center, 1, (0, 100, 100), 2)
             # circle outline
             radius = i[2]
-            cv.circle(src, center, radius, (0, 255, 0), 3)
+            cv2.circle(src, center, radius, (0, 255, 0), 2)
 
-    cv.imshow("detected circles", src)
-    cv.waitKey(0)
+    cv2.imshow("detected circles", src)
+    cv2.waitKey(0)
 
     return 0
 
