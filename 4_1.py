7cd975377f397da4ffd7c85d33216bae278b848b
Iris <f14106155@gs.ncku.edu.tw>
2023-12-14 00:19:46 +0800

    修改成助教給的模型(輸入為32)

--- 4_1.py
+++ 4_1.py
@@ -4,5 +4,6 @@ import torchvision.models as models
 # 建立一個帶有批量歸一化的 VGG19 模型
 vgg19_bn = models.vgg19_bn(num_classes=10)
 
+
 # 使用 torchsummary.summary 在終端中顯示模型結構
-summary(vgg19_bn, (3, 224, 224))  # 輸入圖像維度 (3, 224, 224)
+summary(vgg19_bn, (3, 32, 32))  # 輸入圖像維度 (3, 224, 224)
