c97c3151c5df056e981b3a54de46b9d091a2402b
Iris <f14106155@gs.ncku.edu.tw>
2023-12-13 19:33:24 +0800

    完成4_1

---
+++ 4_1.py
@@ -0,0 +1,8 @@
+from torchsummary import summary
+import torchvision.models as models
+
+# 建立一個帶有批量歸一化的 VGG19 模型
+vgg19_bn = models.vgg19_bn(num_classes=10)
+
+# 使用 torchsummary.summary 在終端中顯示模型結構
+summary(vgg19_bn, (3, 224, 224))  # 輸入圖像維度 (3, 224, 224)
