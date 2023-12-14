000d9debab0a33d77f720b4dada1bd437d2e03ff
Iris <f14106155@gs.ncku.edu.tw>
2023-12-14 15:07:30 +0800

    加上選擇device

--- 4_1.py
+++ 4_1.py
@@ -1,8 +1,10 @@
 from torchsummary import summary
 import torchvision.models as models
+import torch
 
 # 建立一個帶有批量歸一化的 VGG19 模型
-vgg19_bn = models.vgg19_bn(num_classes=10)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+vgg19_bn = models.vgg19_bn(num_classes=10, pretrained=False).to(device)
 
 
 # 使用 torchsummary.summary 在終端中顯示模型結構
