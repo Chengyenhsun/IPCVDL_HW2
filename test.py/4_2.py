4218493f32147f821ff0a706d285153e8ac45847
Iris <f14106155@gs.ncku.edu.tw>
2023-12-21 00:34:45 +0800

    rename testcode資料夾

--- test.py/4_2.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-image_path = "learning_history.jpg"
-image = cv2.imread(image_path)
-
-cv2.imshow("learning_history", image)
-cv2.waitKey(0)  # 顯示圖片並等待任意按鍵關閉視窗
-cv2.destroyAllWindows()
--- test.py/5_3_show.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-image_path = "Accuracy_Comparison.jpg"
-image = cv2.imread(image_path)
-
-cv2.imshow("Accuracy_Comparison", image)
-cv2.waitKey(0)  # 顯示圖片並等待任意按鍵關閉視窗
-cv2.destroyAllWindows()
